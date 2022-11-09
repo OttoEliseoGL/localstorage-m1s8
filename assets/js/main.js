8d13901d39baf990ae75cb3a235ee7d81aef0824
OttoEliseoGL <otto_eliseo@outlook.es>
2022-11-08 19:32:13 -0600

    ejercicio terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,50 @@
 console.log("Entro al main.js");
+
+// localStorage.setItem("colorPagina", "green");
+// const titulo = document.getElementById("titulo");
+// let color = localStorage.getItem("colorPagina");
+// console.log(color);
+// if (color == null) {
+//     color = "pink"
+// };
+
+// titulo.style.color = color;
+
+// localStorage.clear();
+
+let nombres = [];
+let nombre_previo = "";
+let editando = false;
+
+const nombreInput = document.getElementById("nombreInput");
+const lista = document.getElementById("lista");
+
+function agregarNombre() {
+    const nombre = nombreInput.value;
+    if (editando == true) {
+
+    } else {
+        nombres.push(nombre);
+        localStorage.setItem("nombres", JSON.stringify(nombres));
+        nombreInput.value = "";
+        actualizarLista();
+    }
+}
+
+function actualizarLista() {
+    lista.innerHTML = "";
+    console.log("Entrao a actualizarLista")
+    nombres.forEach(nombre => {
+        const li = document.createElement("li");
+        li.textContent = nombre;
+        li.classList.add("list-group-item");
+        lista.appendChild(li);
+
+    });
+}
+
+function limpiarStorage() {
+    localStorage.clear();
+    nombres = [];
+    console.log(nombres);
+}
